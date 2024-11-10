8eb7f1b1e1f68de65bc92e16be06f05f6ce724d0
Matthew Stanciu <mattbstanciu@gmail.com>
2024-11-10 16:17:42 -0500

    remove some projects

--- src/utils/projects-data.ts
+++ src/utils/projects-data.ts
@@ -1,106 +1,85 @@
 export const projects = [
-    {
-        title: "Passports",
-        creator: "Matthew Stanciu / ",
-        creatorLink: "",
-        description: "NFC-embedded booklets serving as the official ID of the Republic of Hackerland.",
-        photo: "/img/passport.jpg",
-        medium: ""
-    },
-    {
-        title: "Phone Bell",
-        creator: "Hazel Roeder",
-        creatorLink: "",
-        description: "Rotary phone-activated doorbell that doubles as an intercom.",
-        photo: "/img/phone.jpg",
-        medium: ""
-    },
-    {
-        title: "The Sign",
-        creator: "Jack Hogan / Hazel Roeder",
-        creatorLink: "",
-        description: "Meter-tall Conway glider that lights up according to Lightning Time (hexidecimal Purdue Hackers time unit).",
-        photo: "/img/sign.jpg",
-        medium: ""
-    },
-    {
-        title: "ETea",
-        creator: "Jadden / ",
-        creatorLink: "",
-        description: "Videogame.",
-        photo: "/img/etea.jpg",
-        medium: ""
-    },
-    {
-        title: "Vine Script",
-        creator: "Ayden Bridges",
-        creatorLink: "",
-        description: "script of vine.",
-        photo: "/img/vine.jpg",
-        medium: ""
-    },
-    {
-        title: "Badges",
-        creator: "Sean Choi / Every single other person who has made a badge",
-        creatorLink: "",
-        description: "Custom designed community tokens laser-cut every Hack Night.",
-        photo: "",
-        medium: ""
-    },
-    {
-        title: "\"Prisha shader\"",
-        creator: "Prisha",
-        creatorLink: "",
-        description: "Ask for a description.",
-        photo: "",
-        medium: ""
-    },
-    {
-        title: "Vulcan",
-        creator: "Lillith",
-        creatorLink: "",
-        description: "Server.",
-        photo: "",
-        medium: ""
-    },
-    {
-        title: "Door opener",
-        creator: "Hazel Roeder",
-        creatorLink: "",
-        description: "Passport-activated door opener.",
-        photo: "",
-        medium: ""
-    },
-    {
-        title: "Something with receipt printers",
-        creator: "Hazel Roeder",
-        creatorLink: "",
-        description: "Receipt printers printing transcription of live gallery audio.",
-        photo: "",
-        medium: ""
-    },
-    {
-        title: "Kart’s hackertyper packet loss installation",
-        creator: "Kart",
-        creatorLink: "",
-        description: "Kart’s hackertyper packet loss installation",
-        photo: "",
-        medium: ""
-    },
-    {
-        title: "Qter",
-        creator: "I don't know",
-        creatorLink: "",
-        description: "Rubik's cube computer",
-        photo: "",
-        medium: ""
-    },
-    {
-        title: "",
-        creator: "",
-        creatorLink: "",
-        description: "",
-        photo: "",
-        medium: ""
-    }
+  {
+    title: "Passports",
+    creator: "Matthew Stanciu / ",
+    creatorLink: "",
+    description:
+      "NFC-embedded booklets serving as the official ID of the Republic of Hackerland.",
+    photo: "/img/passport.jpg",
+    medium: "",
+  },
+  {
+    title: "Phone Bell",
+    creator: "Hazel Roeder",
+    creatorLink: "",
+    description: "Rotary phone-activated doorbell that doubles as an intercom.",
+    photo: "/img/phone.jpg",
+    medium: "",
+  },
+  {
+    title: "The Sign",
+    creator: "Jack Hogan / Hazel Roeder",
+    creatorLink: "",
+    description:
+      "Meter-tall Conway glider that lights up according to Lightning Time (hexidecimal Purdue Hackers time unit).",
+    photo: "/img/sign.jpg",
+    medium: "",
+  },
+  {
+    title: "ETea",
+    creator: "Jadden / ",
+    creatorLink: "",
+    description: "Videogame.",
+    photo: "/img/etea.jpg",
+    medium: "",
+  },
+  {
+    title: "Vine Script",
+    creator: "Ayden Bridges",
+    creatorLink: "",
+    description: "script of vine.",
+    photo: "/img/vine.jpg",
+    medium: "",
+  },
+  {
+    title: "Badges",
+    creator: "Sean Choi / Every single other person who has made a badge",
+    creatorLink: "",
+    description: "Custom designed community tokens laser-cut every Hack Night.",
+    photo: "",
+    medium: "",
+  },
+  {
+    title: "Door opener",
+    creator: "Hazel Roeder",
+    creatorLink: "",
+    description: "Passport-activated door opener.",
+    photo: "",
+    medium: "",
+  },
+  {
+    title: "Receipt printer",
+    creator: "Hazel Roeder",
+    creatorLink: "",
+    description:
+      "Receipt printers printing transcription of live gallery audio.",
+    photo: "",
+    medium: "",
+  },
+  {
+    title: "keymashd",
+    creator: "Kart",
+    creatorLink: "",
+    description: "Kart’s hackertyper packet loss installation",
+    photo: "",
+    medium: "",
+  },
+  {
+    title: "",
+    creator: "",
+    creatorLink: "",
+    description: "",
+    photo: "",
+    medium: "",
+  },
 ];
