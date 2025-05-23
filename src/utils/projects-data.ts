49fe1198b93ece5a924c7cbd9c9313cd700afa6e
Matthew Stanciu <mattbstanciu@gmail.com>
2025-05-22 20:04:05 -0400

    Add recap page (#7)
    * recap
    
    * make it the home page
    
    * run prettier

--- src/utils/projects-data.ts
+++ src/utils/projects-data.ts
@@ -2,7 +2,6 @@ export const projects = [
   {
     title: "Passports",
     creator: "Community",
-    creatorLink: "",
     description:
       "Handmade, faux-leather–bound booklets with embedded NFC chips. Official IDs for the Republic of Hackerland.",
     photo: "/img/passport.jpg",
@@ -10,14 +9,13 @@ export const projects = [
   {
     title: "Phone Bell",
     creator: "Hazel Roeder",
-    creatorLink: "",
-    description: "A rotary phone that rings the doorbell to Hack Night by calling a second phone inside.",
+    description:
+      "A rotary phone that rings the doorbell to Hack Night by calling a second phone inside.",
     photo: "/img/phone.jpg",
   },
   {
     title: "The Sign",
     creator: "Jack Hogan",
-    creatorLink: "",
     description:
       "A meter-tall Conway glider that lights up according to a color representation of the current time.",
     photo: "/img/sign.jpg",
@@ -25,35 +23,34 @@ export const projects = [
   {
     title: "ETea",
     creator: "Jadden Picardal, Saahil Aneja, Alicia Zhou, Mason Graves",
-    creatorLink: "",
-    description: "A video game following the journey of an alien bobarista far from home.",
+    description:
+      "A video game following the journey of an alien bobarista far from home.",
     photo: "/img/burstea.png",
   },
   {
     title: "Vine Script",
     creator: "Ayden Bridges",
-    creatorLink: "",
-    description: "A spoken language with a writing system consisting of vines with leaves & berries.",
+    description:
+      "A spoken language with a writing system consisting of vines with leaves & berries.",
     photo: "/img/vine.jpg",
   },
   {
     title: "Door Opener",
     creator: "Hazel Roeder",
-    creatorLink: "",
-    description: "A device that reads NFC data from a scanned passport & opens the door to Hack Night.",
+    description:
+      "A device that reads NFC data from a scanned passport & opens the door to Hack Night.",
     photo: "/img/door-opener.png",
   },
   {
     title: "Badges",
     creator: "Community",
-    creatorLink: "",
-    description: "Colorful, laser-cut collectibles made & distributed at Hack Night every week.",
+    description:
+      "Colorful, laser-cut collectibles made & distributed at Hack Night every week.",
     photo: "/img/all-badges.jpg",
   },
   {
     title: "Awestruck Printer",
     creator: "Hazel Roeder",
-    creatorLink: "",
     description:
       "A receipt printer that continuously prints data from the other installations onto an increasingly large pile on the floor.",
     photo: "",
@@ -61,8 +58,93 @@ export const projects = [
   {
     title: "keymashd",
     creator: "Kartavya Vashishtha",
-    creatorLink: "",
-    description: "A livestream whose packets are controlled by a typing test. The slower you type, the more packets get dropped.",
+    description:
+      "A livestream whose packets are controlled by a typing test. The slower you type, the more packets get dropped.",
     photo: "",
+  },
+];
+
+export const recapProjects = [
+  {
+    title: "Badges",
+    creator: "Community",
+    description:
+      "Colorful, laser-cut collectibles made & distributed at Hack Night every week. Each one is made by a different person & engraved with the date of its respective Hack Night.",
+    photo: "/img/recap/badges.jpg",
+    cta: "Read blog post",
+    ctaLink: "https://blog.purduehackers.com/posts/the-best-badge",
+  },
+  {
+    title: "Passports",
+    creator: "Matthew Stanciu",
+    description:
+      "Handmade, faux-leather–bound booklets with embedded NFC chips. Official IDs for the Republic of Hackerland.",
+    photo: "/img/recap/passports.jpg",
+    cta: "Read blog post",
+    ctaLink: "https://blog.purduehackers.com/posts/papers-please",
+  },
+  {
+    title: "keymashed",
+    creator: "Kartavya Vashishtha",
+    description:
+      "A livestream whose packets are controlled by a typing test. The slower you type, the more packets get dropped.",
+    photo: "/img/recap/keymashed.jpg",
+    cta: "View on GitHub",
+    ctaLink: "https://github.com/kartva/keymashed",
+  },
+  {
+    title: "Phone Bell",
+    creator: "Hazel Roeder",
+    description:
+      "A rotary phone that rings the doorbell to Hack Night by calling a second phone inside.",
+    photo: "/img/recap/phone.jpg",
+    cta: "",
+    ctaLink: "",
+  },
+  {
+    title: "The Sign",
+    creator: "Jack Hogan",
+    description:
+      "A meter-tall Conway glider that lights up according to a color representation of the current time.",
+    photo: "/img/recap/sign.jpg",
+    cta: "Read blog post",
+    ctaLink: "https://blog.purduehackers.com/posts/sign",
+  },
+  {
+    title: "Vine Script",
+    creator: "Ayden Bridges",
+    description:
+      "A spoken language with a writing system consisting of vines with leaves & berries.",
+    photo: "/img/recap/vine.jpg",
+    cta: "",
+    ctaLink: "",
+  },
+  {
+    title: "ETea",
+    creator: "Jadden Picardal, Saahil Aneja, Alicia Zhou, Mason Graves",
+    description:
+      "A video game following the journey of an alien bobarista far from home.",
+    photo: "/img/recap/etea.jpg",
+    cta: "View on Steam",
+    ctaLink: "https://store.steampowered.com/app/3085040/ETea/",
+  },
+  {
+    title: "Door Opener",
+    creator: "Hazel Roeder",
+    description:
+      "A device that reads NFC data from a scanned passport & opens the door to Hack Night.",
+    photo: "/img/recap/door-opener.jpg",
+    cta: "Read blog post",
+    ctaLink:
+      "https://blog.purduehackers.com/posts/papers-please#:~:text=Another%20project%20built%20on%20top,those%20who%20scan%20their%20passport.",
+  },
+  {
+    title: "Awestruck Printer",
+    creator: "Hazel Roeder",
+    description:
+      "A receipt printer that continuously prints live data from the other installations, as well as surrealist brain dumps from Discord, onto an increasingly large pile on the floor.",
+    photo: "/img/recap/awestruck-printer.jpg",
+    cta: "",
+    ctaLink: "",
   },
 ];
